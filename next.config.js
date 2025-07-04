b1b154db550eb10d4800d6fa0ba0cfa18983e177
tecotv2025 <tecotv2025@gmail.com>
2025-07-04 18:02:20 +0300

    Film detay sayfası eklendi ve resim hostu next.config.js'e tanimlandi

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org'], // TMDB resim sunucusunu buraya ekleyin
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org', // TMDB görselleri için
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com', // YouTube thumbnail'ları veya diğer resimleri için
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
+  // ... diğer Next.js ayarları
 };
 
 module.exports = nextConfig;
